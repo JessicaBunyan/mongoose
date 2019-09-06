84b754b695c33aca9c8803d0db05286faa8afa0e
Jessica Bunyan <jessica.bunyan@megabuyte.com>
2019-09-06 14:36:18 +0100

    monogodb notes

--- server/models/standup.js
+++ server/models/standup.js
@@ -1,19 +1,28 @@
 const mongoose = require("mongoose");
 
 
+
+const requiredStringValidator = [
+	function(val) {
+		let testVal = val.trim();
+		return testVal.length > 0;
+	},
+	"Please supply a value for {PATH}"
+]
+
 const standupSchema = new mongoose.Schema({
     teamMemberId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "teamMembers"
+        ref: "teamMembers",
     },
-    teamMember: {type: String},
-    project: {type: String},
-    workYesterday: {type: String},
-    workToday: {type: String},
-    impediments: {type: String},
+    teamMember: {type: String, validate: requiredStringValidator},
+    project: {type: String, validate: requiredStringValidator},
+    workYesterday: {type: String, validate: requiredStringValidator},
+    workToday: {type: String, validate: requiredStringValidator},
+    impediments: {type: String, validate: requiredStringValidator},
     createdOn: {type: Date, default: Date.now}
 })
-
+ 
 
 
 module.exports = mongoose.model("Standup", standupSchema)
--- server/models/teamMember.js
+++ server/models/teamMember.js
@@ -1,7 +1,15 @@
 const mongoose = require ("mongoose")
 
+
+const sizeValidator = [
+	function(value){
+		return (value.length > 0 && value.length <= 50)
+	},
+	"Error, length must be between 1 an 50 chars long"
+]
+
 const teamMemberSchema = new mongoose.Schema({
-    name: String
+    name: {type: String, required: true}
 })
 
 module.exports = mongoose.model("TeamMember", teamMemberSchema);
